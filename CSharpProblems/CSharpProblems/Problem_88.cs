1e49a294b6d4f7bd37fa9e3cacc154ed171c37fe
jinyell <the.jinyoung.ko@gmail.com>
2019-07-07 09:21:25 -0700

    Problem 88: Merge Sorted Array

---
+++ CSharpProblems/CSharpProblems/Problem_88.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Problem 88: Merge Sorted Array
+ * Difficulty: Easy
+ * 
+ * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+ * 
+ * Note:
+ *     The number of elements initialized in nums1 and nums2 are m and n respectively.
+ *     You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+ * 
+ * Example:
+ * Input:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * 
+ * Output: [1,2,2,3,5,6]
+ */
+
+using System.Collections.Generic;
+
+namespace CSharpProblems
+{
+    public class Problem_88
+    {
+        public class Solution
+        {
+            public void Merge(int[] nums1, int m, int[] nums2, int n)
+            {
+                if (n <= 0)
+                {
+                    return;
+                }
+
+                int index1 = m - 1;
+                int index2 = n - 1;
+                int i = nums1.Length - 1;
+
+                while (index2 >= 0)
+                {
+                    if (index1 >= 0 && nums1[index1] > nums2[index2])
+                    {
+                        nums1[i] = nums1[index1--];
+                    }
+                    else
+                    {
+                        nums1[i] = nums2[index2--];
+                    }
+                    i--;
+                }
+            }
+        }
+    }
+}

f3cee446761a1e707de3e51658d10cbca9ea69dc
jinyell <the.jinyoung.ko@gmail.com>
2019-07-09 09:10:26 -0700

    Problem 101: Symmetric Tree

---
+++ CSharpProblems/CSharpProblems/Problem_101.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * Problem 101: Symmetric Tree
+ * Difficulty: Easy
+ * 
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * 
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 
+ * But the following [1,2,2,null,3,null,3] is not:
+ * 
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ */
+
+namespace CSharpProblems
+{
+    public class Problem_101
+    {
+        /**
+         * Definition for a binary tree node.
+         * public class TreeNode {
+         *     public int val;
+         *     public TreeNode left;
+         *     public TreeNode right;
+         *     public TreeNode(int x) { val = x; }
+         * }
+         */
+        public class Solution
+        {
+            public bool IsSymmetric(TreeNode root)
+            {
+                if (root == null)
+                {
+                    return true;
+                }
+
+                return Symmetrical(root.left, root.right);
+            }
+
+            public bool Symmetrical(TreeNode leftNode, TreeNode rightNode)
+            {
+                bool result = false;
+                if (leftNode == null && rightNode == null)
+                {
+                    result = true;
+                }
+
+                if (leftNode != null && rightNode != null)
+                {
+                    result = (leftNode.val == rightNode.val) &&
+                            Symmetrical(leftNode.left, rightNode.right) &&
+                            Symmetrical(leftNode.right, rightNode.left);
+                }
+                return result;
+            }
+        }
+    }
+}

38053f078912432b9a6c9bdd5b5b1a598cfad2de
jinyell <the.jinyoung.ko@gmail.com>
2020-03-08 17:03:55 -0700

    Problem 1165: Single-Row Keyboard

---
+++ CSharpProblems/CSharpProblems/Problem_1165_Approach_2.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Problem 1165: Single-Row Keyboard
+ * Difficulty: Easy
+ * 
+ * There is a special keyboard with all keys in a single row.
+ * 
+ * Given a string keyboard of length 26 indicating the layout of the keyboard 
+ * (indexed from 0 to 25), initially your finger is at index 0. To type a character, 
+ * you have to move your finger to the index of the desired character. The time 
+ * taken to move your finger from index i to index j is |i - j|.
+ * 
+ * You want to type a string word. Write a function to calculate how much time it takes to type it with one finger.
+ * 
+ * Example 1:
+ *      Input: keyboard = "abcdefghijklmnopqrstuvwxyz", word = "cba"
+ *      Output: 4
+ *      Explanation: The index moves from 0 to 2 to write 'c' then to 1 to write 'b' then to 0 again to write 'a'.
+ *                   Total time = 2 + 1 + 1 = 4. 
+ * 
+ * Example 2:
+ *      Input: keyboard = "pqrstuvwxyzabcdefghijklmno", word = "leetcode"
+ *      Output: 73
+ *  
+ * Constraints:
+ *      keyboard.length == 26
+ *      keyboard contains each English lowercase letter exactly once in some order.
+ *      1 <= word.length <= 10^4
+ *      word[i] is an English lowercase letter.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace CSharpProblems
+{
+    public class Problem_1165_Approach_2
+    {
+        public class Solution
+        {
+            public int CalculateTime(string keyboard, string word)
+            {
+                Dictionary<char, int> dict = new Dictionary<char, int>();
+                for (int i = 0; i < keyboard.Length; i++)
+                {
+                    dict.Add(keyboard[i], i);
+                }
+
+                int total = 0;
+                int lastPos = 0;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    int diff = Math.Abs(dict[word[i]] - lastPos);
+                    lastPos = dict[word[i]];
+                    total += diff;
+                }
+
+                return total;
+            }
+        }
+    }
+}
+
+/*
+ * Complexity Analysis
+ * 		Time complexity  :
+ * 		Space complexity :
+ */
